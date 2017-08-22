fe7af6a44575fcb171f9702edb228f7dfcd6ef38
ahnayem-cse-sust <ahnayem.sust@gmail.com>
2017-08-22 18:14:55 +0600

    Speech Synthesize

--- Mongoldip Web Browser/Mongoldip Web Browser/Form1.Designer.cs
+++ Mongoldip Web Browser/Mongoldip Web Browser/Form1.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.urlButton = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.urlBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // urlButton
+            // 
+            this.urlButton.Location = new System.Drawing.Point(327, 12);
+            this.urlButton.Name = "urlButton";
+            this.urlButton.Size = new System.Drawing.Size(75, 23);
+            this.urlButton.TabIndex = 0;
+            this.urlButton.Text = "Enter";
+            this.urlButton.UseVisualStyleBackColor = true;
+            this.urlButton.Click += new System.EventHandler(this.urlButton_Click);
+            this.urlButton.Enter += new System.EventHandler(this.button1_Enter);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(28, 52);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(636, 363);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // urlBox
+            // 
+            this.urlBox.Location = new System.Drawing.Point(28, 14);
+            this.urlBox.Name = "urlBox";
+            this.urlBox.Size = new System.Drawing.Size(293, 20);
+            this.urlBox.TabIndex = 2;
+            this.urlBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 446);
+            this.Controls.Add(this.urlBox);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.urlButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.go_url);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button urlButton;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox urlBox;
     }
 }
 
--- Mongoldip Web Browser/Mongoldip Web Browser/Form1.cs
+++ Mongoldip Web Browser/Mongoldip Web Browser/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Threading;
 
 namespace Mongoldip_Web_Browser
 {
@@ -16,5 +19,49 @@ namespace Mongoldip_Web_Browser
         {
             InitializeComponent();
         }
+
+        private void button1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        String full_html_text;
+
+        private void urlButton_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(urlBox.Text);
+            String demoText = urlBox.Text + " is loading.";
+            say(demoText);
+            
+            say("You should Filish");
+            full_html_text = webBrowser1.DocumentText;
+            
+        }
+
+        private void go_url(object sender, KeyPressEventArgs e)
+        {
+           
+        }
+
+        SpeechSynthesizer sSynth = new SpeechSynthesizer();
+        PromptBuilder pBuilder = new PromptBuilder();
+        SpeechRecognitionEngine sRecognize = new SpeechRecognitionEngine();
+
+        public void say(String toSay)
+        {
+            pBuilder.ClearContent();
+            pBuilder.AppendText(toSay);
+            sSynth.Speak(pBuilder);
+        }
+
+        private void load(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                urlButton_Click(this, e);
+            }
+        }
+
+        
     }
 }
